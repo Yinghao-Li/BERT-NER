356ad9a9e81a816664e1280b215dee7b4e6595f9
yli3100 <yli3100@gatech.edu>
2020-12-18 16:00:38 -0500

    update script

--- run_mstc_self.sh
+++ run_mstc_self.sh
@@ -19,12 +19,56 @@ TEACHER_UPDATE_PERIOD=2
 OUTPUT_DIR=./MSTC-self-threshold
 MODEL=allenai/scibert_scivocab_uncased
 
+#for SEED in 24601 234 123
+#do
+#  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
+#      --data_dir ../data/ \
+#      --dataset_name $DATASET \
+#      --weak_src nhmm \
+#      --model_name_or_path $MODEL \
+#      --output_dir $OUTPUT_DIR \
+#      --max_seq_length $MAX_SEQ_LEN \
+#      --num_train_epochs $EPOCH \
+#      --per_device_train_batch_size $BATCH_SIZE \
+#      --per_device_eval_batch_size $BATCH_SIZE \
+#      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
+#      --teacher_update_period $TEACHER_UPDATE_PERIOD \
+#      --save_steps 999999999 \
+#      --seed $SEED \
+#      --do_train \
+#      --do_eval \
+#      --do_predict \
+#      --overwrite_output_dir
+#done
+#
+#for SEED in 24601 234 123
+#do
+#  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
+#      --data_dir ../data/ \
+#      --dataset_name $DATASET \
+#      --weak_src hmm \
+#      --model_name_or_path $MODEL \
+#      --output_dir $OUTPUT_DIR \
+#      --max_seq_length $MAX_SEQ_LEN \
+#      --num_train_epochs $EPOCH \
+#      --per_device_train_batch_size $BATCH_SIZE \
+#      --per_device_eval_batch_size $BATCH_SIZE \
+#      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
+#      --teacher_update_period $TEACHER_UPDATE_PERIOD \
+#      --save_steps 999999999 \
+#      --seed $SEED \
+#      --do_train \
+#      --do_eval \
+#      --do_predict \
+#      --overwrite_output_dir
+#done
+
 for SEED in 24601 234 123
 do
   CUDA_VISIBLE_DEVICES=$1 python self_train.py \
       --data_dir ../data/ \
       --dataset_name $DATASET \
-      --weak_src nhmm \
+      --weak_src majority \
       --model_name_or_path $MODEL \
       --output_dir $OUTPUT_DIR \
       --max_seq_length $MAX_SEQ_LEN \
@@ -46,7 +90,7 @@ do
   CUDA_VISIBLE_DEVICES=$1 python self_train.py \
       --data_dir ../data/ \
       --dataset_name $DATASET \
-      --weak_src hmm \
+      --weak_src iid \
       --model_name_or_path $MODEL \
       --output_dir $OUTPUT_DIR \
       --max_seq_length $MAX_SEQ_LEN \
