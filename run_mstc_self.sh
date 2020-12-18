b764fad76bb485278fce49491fc623776503e10d
yli3100 <yli3100@gatech.edu>
2020-12-18 14:17:50 -0500

    bug fix

---
+++ run_mstc_self.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Author: Yinghao Li @ Georgia Tech ECE
+# All rights reserved
+
+# -------------------------------------
+# This script is used to run txt_gen_train.py
+# -------------------------------------
+
+# Quit if there's any errors
+set -e
+
+DATASET=MSTC
+EPOCH=50
+BATCH_SIZE=10
+MAX_SEQ_LEN=480
+SELF_TRAINING_START_EPOCH=20
+TEACHER_UPDATE_PERIOD=2
+OUTPUT_DIR=./MSTC-self-threshold
+MODEL=allenai/scibert_scivocab_uncased
+
+for SEED in 24601 234 123
+do
+  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
+      --data_dir ../data/ \
+      --dataset_name $DATASET \
+      --weak_src nhmm \
+      --model_name_or_path $MODEL \
+      --output_dir $OUTPUT_DIR \
+      --max_seq_length $MAX_SEQ_LEN \
+      --num_train_epochs $EPOCH \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
+      --teacher_update_period $TEACHER_UPDATE_PERIOD \
+      --save_steps 999999999 \
+      --seed $SEED \
+      --do_train \
+      --do_eval \
+      --do_predict \
+      --overwrite_output_dir
+done
+
+for SEED in 24601 234 123
+do
+  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
+      --data_dir ../data/ \
+      --dataset_name $DATASET \
+      --weak_src hmm \
+      --model_name_or_path $MODEL \
+      --output_dir $OUTPUT_DIR \
+      --max_seq_length $MAX_SEQ_LEN \
+      --num_train_epochs $EPOCH \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
+      --teacher_update_period $TEACHER_UPDATE_PERIOD \
+      --save_steps 999999999 \
+      --seed $SEED \
+      --do_train \
+      --do_eval \
+      --do_predict \
+      --overwrite_output_dir
+done
