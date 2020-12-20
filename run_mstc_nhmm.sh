226bcca07f4eb847dab1d56356119f66c1c58b9e
yli3100 <yli3100@gatech.edu>
2020-12-20 16:37:20 -0500

    fix bugs, implement early stopping

--- run_mstc_nhmm.sh
+++ run_mstc_nhmm.sh
@@ -36,3 +36,23 @@ do
       --do_predict \
       --overwrite_output_dir
 done
+
+for SEED in 0 42 24601 234 476
+do
+  CUDA_VISIBLE_DEVICES=$1 python bert_ner.py \
+      --data_dir ../data/ \
+      --dataset_name $DATASET \
+      --weak_src hmm \
+      --model_name_or_path $MODEL \
+      --output_dir $OUTPUT_DIR \
+      --max_seq_length $MAX_SEQ_LEN \
+      --num_train_epochs $EPOCH \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --save_steps 999999999 \
+      --seed $SEED \
+      --do_train \
+      --do_eval \
+      --do_predict \
+      --overwrite_output_dir
+done
--- run_mstc_self.sh
+++ run_mstc_self.sh
@@ -14,54 +14,54 @@ DATASET=MSTC
 EPOCH=50
 BATCH_SIZE=8
 MAX_SEQ_LEN=480
-SELF_TRAINING_START_EPOCH=20
+SELF_TRAINING_START_EPOCH=30
 TEACHER_UPDATE_PERIOD=2
 OUTPUT_DIR=./MSTC-self-threshold
 MODEL=allenai/scibert_scivocab_uncased
 
-#for SEED in 24601 234 123
-#do
-#  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
-#      --data_dir ../data/ \
-#      --dataset_name $DATASET \
-#      --weak_src nhmm \
-#      --model_name_or_path $MODEL \
-#      --output_dir $OUTPUT_DIR \
-#      --max_seq_length $MAX_SEQ_LEN \
-#      --num_train_epochs $EPOCH \
-#      --per_device_train_batch_size $BATCH_SIZE \
-#      --per_device_eval_batch_size $BATCH_SIZE \
-#      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
-#      --teacher_update_period $TEACHER_UPDATE_PERIOD \
-#      --save_steps 999999999 \
-#      --seed $SEED \
-#      --do_train \
-#      --do_eval \
-#      --do_predict \
-#      --overwrite_output_dir
-#done
-#
-#for SEED in 24601 234 123
-#do
-#  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
-#      --data_dir ../data/ \
-#      --dataset_name $DATASET \
-#      --weak_src hmm \
-#      --model_name_or_path $MODEL \
-#      --output_dir $OUTPUT_DIR \
-#      --max_seq_length $MAX_SEQ_LEN \
-#      --num_train_epochs $EPOCH \
-#      --per_device_train_batch_size $BATCH_SIZE \
-#      --per_device_eval_batch_size $BATCH_SIZE \
-#      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
-#      --teacher_update_period $TEACHER_UPDATE_PERIOD \
-#      --save_steps 999999999 \
-#      --seed $SEED \
-#      --do_train \
-#      --do_eval \
-#      --do_predict \
-#      --overwrite_output_dir
-#done
+for SEED in 24601 234 123
+do
+  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
+      --data_dir ../data/ \
+      --dataset_name $DATASET \
+      --weak_src nhmm \
+      --model_name_or_path $MODEL \
+      --output_dir $OUTPUT_DIR \
+      --max_seq_length $MAX_SEQ_LEN \
+      --num_train_epochs $EPOCH \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
+      --teacher_update_period $TEACHER_UPDATE_PERIOD \
+      --save_steps 999999999 \
+      --seed $SEED \
+      --do_train \
+      --do_eval \
+      --do_predict \
+      --overwrite_output_dir
+done
+
+for SEED in 24601 234 123
+do
+  CUDA_VISIBLE_DEVICES=$1 python self_train.py \
+      --data_dir ../data/ \
+      --dataset_name $DATASET \
+      --weak_src hmm \
+      --model_name_or_path $MODEL \
+      --output_dir $OUTPUT_DIR \
+      --max_seq_length $MAX_SEQ_LEN \
+      --num_train_epochs $EPOCH \
+      --per_device_train_batch_size $BATCH_SIZE \
+      --per_device_eval_batch_size $BATCH_SIZE \
+      --self_training_start_epoch $SELF_TRAINING_START_EPOCH \
+      --teacher_update_period $TEACHER_UPDATE_PERIOD \
+      --save_steps 999999999 \
+      --seed $SEED \
+      --do_train \
+      --do_eval \
+      --do_predict \
+      --overwrite_output_dir
+done
 
 for SEED in 24601 234 123
 do
